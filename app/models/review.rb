59439a84d35ad471de8ccf770a6014b4a8641ceb
Kit Trowbridge <kittrow.dev@gmail.com>
2025-01-08 07:28:32 -0500

    added validations to restaurant model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,10 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
 end
+
+# Validations:
+# A review must belong to a restaurant.
+# A review must have a content.
+# A review must have a rating.
+# A review’s rating must be a number between 0 and 5.
+# A review’s rating must be an integer. For example, a review with a rating of 2.5 should be invalid!
