624189b02f4de6077347a9c7b258bdab8272b788
Kit Trowbridge <kittrow.dev@gmail.com>
2025-01-08 09:15:32 -0500

    installed faker and seeded 20 restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,21 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+category = %w[chinese italian japanese french belgian]
+
+# puts 'Cleaning database...'
+# Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+
+10.times do
+  Restaurant.create(
+    name: "The #{Faker::Creature::Horse.name} #{Faker::Creature::Animal.name.capitalize}",
+    address: Faker::Address.city,
+    category: category.sample,
+    phone_number: Faker::PhoneNumber.phone_number
+  )
+  # this interpolation isn't working
+  puts "Created #{Restaurant.name}"
+end
+
+puts "Finished! Created #{Restaurant.count} restaurants"
